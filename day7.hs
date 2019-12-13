f9e3e43f64d68c1742b3be952c45d095b2ae08cb
Bryan Richter <bryan.richter@relexsolutions.com>
2019-12-13 19:37:57 +0200

    Day 7, provisional

--- day7.hs
+++ day7.hs
@@ -6,6 +6,7 @@
 
 import Data.Function
 import Data.List
+import Data.Maybe
 import Data.Map (Map)
 import qualified Data.Map as M
 
@@ -70,7 +71,7 @@ data Computer = Computer
     { computerStatus :: Status
     , computerMemory :: Memory
     , computerInput :: [Int]
-    , computerOutput :: [Int]
+    , computerOutput :: Maybe Int
     } deriving (Eq, Show)
 
 evalStep :: Computer -> Computer
@@ -80,13 +81,13 @@ evalStep (Computer _ mem@(Memory ram pc) ins outs)
         Nothing -> Computer Error mem ins outs
         Just (Op Add ps) -> Computer Running (add mem ps) ins outs
         Just (Op Mul ps) -> Computer Running (mul mem ps) ins outs
-        Just (Op Input ps) -> 
+        Just (Op Input ps) ->
             case ins of
                 [] -> error "no input for Input"
                 (i:ii) -> Computer Running (inp mem ps i) ii outs
         Just (Op Output ps) ->
             let (newmem, o) = out mem ps
-            in Computer Running newmem ins (o:outs)
+            in Computer Running newmem ins (Just o)
         Just (Op JumpT ps) -> Computer Running (jmpt mem ps) ins outs
         Just (Op JumpF ps) -> Computer Running (jmpf mem ps) ins outs
         Just (Op StoreLT ps) -> Computer Running (stolt mem ps) ins outs
@@ -145,34 +146,49 @@ isRunning :: Computer -> Bool
 isRunning (Computer Running _ _ _) = True
 isRunning _ = False
 
-runProgram :: Program -> [Int] -> ([Int],[Int])
+runProgram :: Program -> [Int] -> [Int]
 runProgram program input =
     let mem0 = Memory (M.fromList (zip [0..] program)) 0
         step nxt c
-            | isRunning c = nxt (evalStep c)
-            | otherwise = pure c
-    in do
-        c@(Computer resStat resRam _ outs) <- fix step (Computer Running mem0 input [])
-        case resStat of
-            -- elems gives ascending order
-            Halted -> (M.elems (memRam resRam), outs)
-            _ -> error (show c)
+            | isRunning c = computerOutput c : nxt (evalStep (c { computerOutput = Nothing }))
+            | otherwise = [computerOutput c]
+    in catMaybes $ fix step (Computer Running mem0 input Nothing)
 
 type PhaseSetting = Int
 type Program = [Int]
 
 runAmpCircuit :: Program -> [PhaseSetting] -> Int
-runAmpCircuit prog = 
+runAmpCircuit prog =
     -- folding over the phasesetting list.
     -- the accumulator is the output of the previous amp.
     -- acc0 is 0.
-    foldl' (\input phase -> head (snd (runProgram prog [phase, input]))) 0
-    
-main = do
+    foldl' (\input phase -> head (runProgram prog [phase, input])) 0
+
+main0 = do
     prog <- readProgramFile "day7-input"
     print $ maximum $ map (runAmpCircuit prog) $ permutations [0..4]
 
+main = do
+    prog <- readProgramFile "day7-input"
+    print $ maximum $ map (runFeedbackAmpCircuit prog) $ permutations [5..9]
+
 readProgramFile f = read @[Int] . ("["++) . (++"]") . head . lines <$> readFile f
 
-isRight (Right _) = True
-isRight _ = False
+runFeedbackAmpCircuit :: Program -> [PhaseSetting] -> Int
+runFeedbackAmpCircuit prog phases =
+    let input0 = (phases !! 0) : 0 : outputs4
+        input1 = (phases !! 1) : outputs0
+        input2 = (phases !! 2) : outputs1
+        input3 = (phases !! 3) : outputs2
+        input4 = (phases !! 4) : outputs3
+        outputs0 = runProgram prog input0
+        outputs1 = runProgram prog input1
+        outputs2 = runProgram prog input2
+        outputs3 = runProgram prog input3
+        outputs4 = runProgram prog input4
+    in last outputs4
+
+dummy2, dummy :: Program
+dummy = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+
+dummy2 = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54, -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4, 53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
