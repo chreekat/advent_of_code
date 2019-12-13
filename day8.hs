e0005904460a8d51501dea1376d7b724284af224
Bryan Richter <bryan.richter@relexsolutions.com>
2019-12-13 19:49:05 +0200

    Start day 8

---
+++ day8.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wall -Wno-missing-signatures -Wno-unused-top-binds #-}
