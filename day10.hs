61af7da15e8e13f8cc73ed3a48f5ff4dd7d17b03
Bryan Richter <bryan.richter@relexsolutions.com>
2019-12-16 21:52:52 +0200

    Day 10

--- day10.hs
+++ day10.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# OPTIONS_GHC -Wall -Wno-missing-signatures -Wno-unused-top-binds #-}
 
+import Control.Arrow
 import Data.Function
 import Data.List
 import Data.Ratio
@@ -14,17 +15,16 @@ import qualified Data.Map as M
 --import Debug.Pretty.Simple
 
 main = do
-    part1
-    part2
-
-part1 = pure ()
-part2 = pure ()
+    chart <- readChart <$> readFile "day10-input"
+    let res1 = maxDetections chart
+        (Asteroid x y) = makeThemSuffer (takeRangedBearings chart (fst res1)) !! 199
+    print res1
+    print (x * 100 + y)
 
 data Asteroid = Asteroid { ax :: Int, ay :: Int }
     deriving (Eq, Ord, Show)
 
-data Chart = Chart { cx :: Int, cy :: Int, cAsteroid :: Set Asteroid }
-    deriving (Eq, Show)
+type Chart = Set Asteroid
 
 readAsteroid :: Int -> Int -> Char -> Maybe Asteroid
 readAsteroid y x = \case
@@ -35,21 +35,56 @@ readAsteroid y x = \case
 readChart :: String -> Chart
 readChart str =
     let ys = zip [0..] (lines str)
-        h = length ys
-        w = length (snd (head ys))
         xs = concatMap blop ys
         blop (y,s) = zipWith (readAsteroid y) [0..] s
-    in Chart h w (S.fromList (catMaybes xs))
+    in S.fromList (catMaybes xs)
 
-data Bearing = R (Ratio Int) | L (Ratio Int) | U | D
+data Bearing = U | R (Ratio Int) | D |  L (Ratio Int)
     deriving (Eq, Ord, Show)
 
 takeBearings :: Chart -> Asteroid -> Set Bearing
---takeBearings Chart{cAsteroid = ch} a = S.map (\a2 -> let b = takeBearing a a2 in pTraceShow (a2,b) b) (S.delete a ch)
-takeBearings Chart{cAsteroid = ch} a = S.map (takeBearing a) (S.delete a ch)
+takeBearings ch a = S.map (takeBearing a) (S.delete a ch)
+
+type Range = Int
+
+data RangedBearing = RangedBearing Bearing Range
+    deriving (Eq, Show, Ord)
+
+takeRangedBearings :: Chart -> Asteroid -> Map Bearing (Map Range Asteroid)
+takeRangedBearings ch a =
+    let rangedBearings = map (id &&& takeRangedBearing a) (S.toList (S.delete a ch))
+        mkBearingMap (a2, RangedBearing b r) = M.singleton b (M.singleton r a2)
+    in M.unionsWith (<>) (map mkBearingMap rangedBearings)
+
+   where
+
+obliterate :: Bearing -> Map Bearing (Map Range Asteroid) -> (Asteroid, Map Bearing (Map Range Asteroid))
+obliterate b bmap =
+    let targets = bmap M.! b
+        (_,targ) = M.elemAt 0 targets
+        remainder = M.deleteAt 0 targets
+        adjMap = if M.null remainder then M.delete b else M.insert b remainder
+    in (targ, adjMap bmap)
+
+makeThemSuffer :: Map Bearing (Map Range Asteroid) -> [Asteroid]
+makeThemSuffer = wronk U
+  where
+    wronk start m
+        | M.null m = []
+        | otherwise
+        = case M.lookupGE start m of
+            Nothing -> wronk U m
+            Just (b, _) ->
+                let (ded, remaining) = obliterate b m
+                in ded : case M.lookupGT b remaining of
+                    Nothing -> wronk U remaining
+                    Just (b', _) -> wronk b' remaining
 
 takeBearing :: Asteroid -> Asteroid -> Bearing
-takeBearing (Asteroid x0 y0) (Asteroid x1 y1) =
+takeBearing a1 a2 = let (RangedBearing b _) = takeRangedBearing a1 a2 in b
+
+takeRangedBearing :: Asteroid -> Asteroid -> RangedBearing
+takeRangedBearing (Asteroid x0 y0) (Asteroid x1 y1) =
     let bx = x1 - x0
         by = y1 - y0
     in case (compare y1 y0, compare x1 x0) of
@@ -57,13 +92,13 @@ takeBearing (Asteroid x0 y0) (Asteroid x1 y1) =
         {-
         (YY, XX)
         -}
-        (_, LT) -> L (by % bx)
-        (_, GT) -> R (by % bx)
-        (LT, EQ) -> U
-        (GT, EQ) -> D
+        (_, LT) -> RangedBearing (L (by % bx)) (abs bx)
+        (_, GT) -> RangedBearing (R (by % bx)) (abs bx)
+        (LT, EQ) -> RangedBearing U (abs by)
+        (GT, EQ) -> RangedBearing D (abs by)
 
 countDetections :: Chart -> Map Asteroid Int
-countDetections c@(Chart{cAsteroid = ch}) = M.fromSet (S.size . takeBearings c) ch
+countDetections ch = M.fromSet (S.size . takeBearings ch) ch
 
 maxDetections :: Chart -> (Asteroid, Int)
 maxDetections = maximumBy (compare `on` snd) . M.toList . countDetections
