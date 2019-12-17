1e359520c1fdb089c0277e85c005981a36b37bea
Bryan Richter <bryan.richter@relexsolutions.com>
2019-12-17 20:43:14 +0200

    Start day 11

---
+++ day11.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -Wall -Wno-missing-signatures -Wno-unused-top-binds #-}
+
+-- import Control.Arrow
+-- import Data.Function
+-- import Data.List
+-- import Data.Ratio
+-- import Data.Maybe
+-- import Data.Set (Set)
+-- import Data.Map (Map)
+-- import qualified Data.Set as S
+-- import qualified Data.Map as M
+
+main = do
+    inp <- readFile "day11-input"
+    part1 inp
+
+part1 = const (pure ())
