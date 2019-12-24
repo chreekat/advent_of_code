9fb3dc71a1d73169a9ff04afbce9c6017bdd963f
Bryan Richter <bryan.richter@relexsolutions.com>
2019-12-24 07:42:38 +0000

    Day 13 part 1

--- day13.hs
+++ day13.hs
@@ -1,11 +1,22 @@
 import qualified Data.Map as M
 import Data.List
 
+import Text.Pretty.Simple
+
 import Intcode
 
 runGame game =
-    let output = runProgram game
-        stepGame (x,y,c) = M.insert (x,y) c
+    let output = runProgram game []
+        stepGame st (x,y,c) = M.insert (x,y) c st
+        triples [] = []
         triples (x:y:c:rest) = (x,y,c) : triples rest
     in
-    foldl' stepGame M.empty triples
+    foldl' stepGame M.empty (triples output)
+
+main = part1
+
+part1 = do
+    prog <- readProgramFile "day13-input"
+    let sta = runGame prog
+    pPrint (M.size (M.filter (== 2) sta))
+
