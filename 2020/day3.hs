e400bb650ff3f46a7a59cb5daa4cf72ad83cbf21
Bryan Richter <bryan.richter@relexsolutions.com>
2020-12-06 12:05:54 +0200

    Import split to day3

--- 2020/day3.hs
+++ 2020/day3.hs
@@ -1,4 +1,5 @@
 import Control.Monad.Trans.State.Lazy
+import Data.List.Split
 
 main = do
     z <- readFile "day3-input.txt"
@@ -16,25 +17,3 @@ main = do
 mkState h v = traverse (\l -> (modify (\s -> step h s l)))  . map head . chunksOf v
 
 step horiz (i, ct) row = (i + horiz, ct + (row !! (i `mod` length row)))
-{-
-
-
-
-    Right 1, down 1.
-    Right 3, down 1. (This is the slope you already checked.)
-    Right 5, down 1.
-    Right 7, down 1.
-    Right 1, down 2.
-
-
--}
-
--- Stolen from split package
-build :: ((a -> [a] -> [a]) -> [a] -> [a]) -> [a]
-build g = g (:) []
-
-chunksOf :: Int -> [e] -> [[e]]
-chunksOf i ls = map (take i) (build (splitter ls)) where
-  splitter :: [e] -> ([e] -> a -> a) -> a -> a
-  splitter [] _ n = n
-  splitter l c n  = l `c` splitter (drop i l) c n
