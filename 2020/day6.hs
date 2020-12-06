3236a156c17125db0b5cd0ca680ae3f8cdf30d7a
Bryan Richter <bryan.richter@relexsolutions.com>
2020-12-06 13:07:55 +0200

    Code golf day6 with NonEmpty

--- 2020/day6.hs
+++ 2020/day6.hs
@@ -1,32 +1,15 @@
 import Data.List
 import Data.List.Split
-import Data.Maybe
-import Data.Monoid
-import System.IO.Unsafe
+import qualified Data.List.NonEmpty as NE
 
-main = print (part1, part2)
-
-input = unsafePerformIO (readFile "day6-input.txt")
-
-head' :: Foldable t => t a -> Maybe a
-head' = getFirst . foldMap (First . Just)
-
-groupAnswers = splitOn [""] . lines
-
-reducedAnswers1 :: Ord a => [[a]] -> [[a]]
-reducedAnswers1 = group . sort . concat
-
-reducedAnswers2 :: Ord a => [[a]] -> [[a]]
-reducedAnswers2 individualAnswers =
-    let tot = length individualAnswers
-    in filter ((== tot) . length) (reducedAnswers1 individualAnswers)
-
-summarized reducer =
-    sum (map (length . simplifyAnswers . reducer) (groupAnswers input))
-
-simplifyAnswers :: Eq a => [[a]] -> [a]
-simplifyAnswers = catMaybes . map head'
-
-part1 = summarized reducedAnswers1
-
-part2 = summarized reducedAnswers2
+main = do
+    input <- readFile "day6-input.txt"
+    let reducedAnswers1 = NE.group . sort . concat
+        reducedAnswers2 individualAnswers =
+            let tot = length individualAnswers
+            in filter ((== tot) . length) (reducedAnswers1 individualAnswers)
+        summarized reducer =
+            sum . map (length . map NE.head . reducer) . splitOn [""] . lines
+        part1 = summarized reducedAnswers1 input
+        part2 = summarized reducedAnswers2 input
+    print (part1, part2)
