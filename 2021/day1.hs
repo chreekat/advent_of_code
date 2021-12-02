370cf0e6470ef24f63f3cfbe87d8da3b8ef9367d
Bryan Richter <bryan.richter@relexsolutions.com>
2021-12-02 06:22:11 +0200

    Rename dat

--- 2021/day1.hs
+++ 2021/day1.hs
@@ -10,15 +10,15 @@ ex1 = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 puz1 :: [Int] -> _
 puz1 = foldl' step Nothing
 
-day1dat1 :: [Int]
-day1dat1 = map read (lines (unsafePerformIO (readFile "day1.txt")))
+dat :: [Int]
+dat = map read (lines (unsafePerformIO (readFile "day1.txt")))
 
-ans1_1 = fst <$> (puz1 day1dat1)
+ans1_1 = fst <$> (puz1 dat)
 
 theSeq x = map sum3 (zip3 x (tail x) (tail (tail x)))
 
 
-ans1_2 = fst <$> (puz1 (theSeq (day1dat1)))
+ans1_2 = fst <$> (puz1 (theSeq (dat)))
 
 -- ghcid needs this?
 main = undefined
