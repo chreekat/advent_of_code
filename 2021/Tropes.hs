a5996a88e13e4b46c144203849d7941bf5d5984d
Bryan Richter <bryan.richter@relexsolutions.com>
2021-12-04 11:27:58 +0200

    2021.4.1

--- 2021/Tropes.hs
+++ 2021/Tropes.hs
@@ -1,11 +1,19 @@
-module Tropes (traceShow, traceShowId, foldl', transpose , unsafePerformIO, module Tropes) where
+module Tropes (pTraceShow, First(..),pPrint, All(..),intercalate, splitOn, traceShow, traceShowId, foldl', transpose , unsafePerformIO, module Tropes) where
 import Data.List
+import Data.List.Split
 import System.IO.Unsafe
+import Data.Monoid
+import Text.Pretty.Simple
+import Data.Monoid
 
 import Debug.Trace
+import Debug.Pretty.Simple
 
 sum3 (a,b,c) = sum [a,b,c]
 
 prod2 (a,b) = product [a,b]
 
 _3to2 (a,b,c) = (a,b)
+
+pars = splitOn "\n\n"
+unpars = intercalate "\n\n"
--- 2021/day4.hs
+++ 2021/day4.hs
@@ -1,13 +1,87 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 import Tropes
 
-ex1 = []
+ex1, dat :: String
+ex1 = unsafePerformIO (readFile "ex4-1.txt")
 
-dat :: String
 dat = unsafePerformIO (readFile "day4.txt")
 
+
+data MyState = MyState { remaining :: [Int], boards :: [Board] }
+    deriving (Eq, Show)
+
+data Board = Board { lines' :: [[Cell]], allCells :: [Cell] }
+    deriving (Eq, Show)
+
+data Cell = Cell { num :: Int, marked :: Bool }
+    deriving (Eq, Show)
+
+-- UNFOLDING
+--
+
+mkState s =
+    let (p:ps) = pars s
+        rem = map read (splitOn "," p)
+        boards = map mkBoard ps
+    in MyState rem boards
+
+
+mkCell n = Cell n False
+
+mkBoard :: String -> Board
+mkBoard s =
+    let ls   = map (map (mkCell . read) . words) (lines s)
+        cols = map (map (mkCell . read)) $ transpose $ map words (lines s)
+        all = map (mkCell . read) (words s)
+    in Board (ls <> cols) all
+
+
+-- FOLDING
+--
+
+winner :: Board -> Bool
+winner = any (getAll . foldMap (All . marked)) . lines'
+
+newBoard :: Int -> Board -> (Board, Maybe Int)
+newBoard n oldBoard =
+    let newb = mark oldBoard n
+        s = if winner newb
+                then Just (score n newb)
+                else Nothing
+    in (newb, s)
+
+score n = (n *) .  sum . map num . filter (not . marked) . allCells
+
+mark (Board l a) n =
+    let l' = map (markLine n) l
+        a' = map (markCell n) a
+    in Board l' a'
+
+markLine n = map (markCell n)
+
+markCell n c@(Cell num' m) 
+    | num' == n = Cell num' True
+    | otherwise = c
+
+gameOver = getFirst . foldMap (First . snd)
+
+step (MyState ns bs)
+    | [] <- ns = error "Game over!"
+    | (n:rem) <- ns = 
+        let bs' = map (newBoard n) bs
+        in (rem, bs')
+
+playGame st = 
+    let (rem, bs') = step st
+    in case gameOver  bs' of
+        Just s -> {-pTraceShow (MyState rem (map fst bs')) $-} Just s
+        Nothing -> playGame (MyState rem (map fst bs'))
+
+-- WRAP UP
+--
+
 ans1 :: _
-ans1 = undefined
+ans1 = playGame (mkState dat)
 
 ans2 :: _
 ans2 = undefined
