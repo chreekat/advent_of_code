03894f2e27cf8871729678dd5dde1cbfe90e0f26
Bryan Richter <bryan.richter@relexsolutions.com>
2021-12-05 19:35:17 +0200

    2021.5.1

--- 2021/Tropes.hs
+++ 2021/Tropes.hs
@@ -1,7 +1,9 @@
-module Tropes (partition, isJust, isNothing, Last (..), pTraceShow, First (..), pPrint, All (..), intercalate, splitOn, traceShow, traceShowId, foldl', transpose, unsafePerformIO, module Tropes) where
+module Tropes (pTraceShowId, on, sort, fromMaybe, partition, isJust, isNothing, Last (..), pTraceShow, First (..), pPrint, All (..), intercalate, splitOn, traceShow, traceShowId, foldl', transpose, unsafePerformIO, module Tropes) where
 
+import Data.Function
 import Data.List
 import Data.List.Split
+import qualified Data.Map as Map
 import Data.Maybe
 import Data.Monoid
 import System.IO.Unsafe
@@ -18,3 +20,20 @@ _3to2 (a, b, c) = (a, b)
 
 pars = splitOn "\n\n"
 unpars = intercalate "\n\n"
+
+
+range x y = if x > y then [y..x] else [x..y]
+
+mapAlter :: Ord k => (Maybe a -> Maybe a) -> k -> Map.Map k a -> Map.Map k a 
+mapAlter = Map.alter
+
+mapEmpty = Map.empty
+
+mapToList = Map.toList
+
+mapFilter = Map.filter
+
+mapSingleton = Map.singleton
+
+mapUnionsWith :: (Foldable f, Ord k) => (a -> a -> a) -> f (Map.Map k a) -> Map.Map k a 
+mapUnionsWith = Map.unionsWith
--- 2021/day5.hs
+++ 2021/day5.hs
@@ -2,13 +2,47 @@
 
 import Tropes
 
-ex1 = []
 
-dat :: String
+ex1, dat :: String
 dat = unsafePerformIO (readFile "day5.txt")
+ex1 = unsafePerformIO (readFile "ex5-1.txt")
+
+
+data L = L (Int,Int) (Int,Int)
+    deriving (Eq, Show)
+
+pLine s =
+    let [begin,_,end] = words s
+        [[x1,y1],[x2,y2]] = map (map read . splitOn ",") [begin,end]
+    in L (x1,y1) (x2,y2)
+
+pLines = map pLine . lines
+
+
+
+
+
+diagonal (L (x1, y1) (x2, y2)) = x1 /= x2 && y1 /= y2
+
+drawLine d l@(L (x1,y1) (x2,y2))
+    | diagonal l = d
+    | x1 == x2 = foldr (markDiagram x1) d (range y1 y2)
+    | otherwise = foldr (markDiagram `flip` y1) d (range x1 x2)
+
+
+markDiagram x y = mapAlter (Just . maybe 1 succ) (x,y)
+
+
+drawDiagram = foldl' drawLine mapEmpty . pLines
+
+dangerous = mapFilter (> 1)
+
+--
+-- PRETTY
+--
 
 ans1 :: _
-ans1 = undefined
+ans1 = length $ dangerous $ drawDiagram dat
 
 ans2 :: _
 ans2 = undefined
