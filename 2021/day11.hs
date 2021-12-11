ccb5f3ab2562d30b03d1ef1a6bbf877182d6bf13
Bryan Richter <bryan.richter@relexsolutions.com>
2021-12-11 17:14:34 +0200

    Begin 2021.11

---
+++ 2021/day11.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE TupleSections #-}
+
+import Tropes
+
+ex1, dat :: String
+ex1 = unsafePerformIO (readFile "day11-ex1.txt")
+dat = unsafePerformIO (readFile "day11.txt")
+
+ans1 :: _
+ans1 = undefined
+
+ans2 :: _
+ans2 = undefined
+
+-- ghcid needs this?
+main = undefined
