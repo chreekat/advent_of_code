ebb3a1ad1fd93e08aa50db0b75ec69fb4401eed7
Bryan Richter <bryan.richter@relexsolutions.com>
2021-12-13 07:15:23 +0200

    Start 2021.13

---
+++ 2021/day13.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+
+import Tropes
+
+ex1, dat :: String
+ex1 = ""
+dat = unsafePerformIO (readFile "day13.txt")
+
+ans1 :: _
+ans1 = undefined
+
+ans2 :: _
+ans2 = undefined
+
+-- ghcid needs this?
+main = undefined
