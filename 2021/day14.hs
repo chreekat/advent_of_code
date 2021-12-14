64cad5453c20686255602b59382ad9d0c7f56040
Bryan Richter <bryan.richter@relexsolutions.com>
2021-12-14 10:16:20 +0200

    2021.14.1

--- 2021/day14.hs
+++ 2021/day14.hs
@@ -12,7 +12,7 @@ ex1 = unsafePerformIO (readFile "day14-ex1.txt")
 dat = unsafePerformIO (readFile "day14.txt")
 
 
-data I = I { is :: String, im :: [(String, Char)] }
+data I = I { is :: String, im :: Map String Char }
     deriving (Eq, Show)
 
 instance Semigroup I where
@@ -25,14 +25,28 @@ pTmpl s = I s mempty
 
 pRule s = 
     let [k, [v]] = splitOn " -> " s
-    in I "" [(k, v)]
+    in I "" (mapSingleton k v)
 
 pI s =
     let [[tmpl], rules] = splitOn [""] $ lines s
     in mconcat (pTmpl tmpl : map pRule rules)
 
+step :: I -> _
+step I {is, im} = I newIs im where
+    newIs = head is : mconcat (zipWith (expand im) is (tail is))
+
+expand :: Map String Char -> Char -> Char -> String
+expand m c1 c2 = maybe [c2] (: [c2]) (mapLookup [c1,c2] m)
+
+score  I { is } = 
+    let m = mapUnionsWith (<>) (map (\c -> mapSingleton c (Sum 1)) is)
+        (j, k) = (minimum &&& maximum) m
+    in getSum $ k - j
+
+scoreSteps n s = score $ last $ take (n+1) $ iterate step (pI s)
+
 ans1 :: _
-ans1 = undefined
+ans1 = scoreSteps 10 dat
 
 ans2 :: _
 ans2 = undefined
--- 2021/template.hs
+++ 2021/template.hs
@@ -8,7 +8,7 @@
 import Tropes
 
 ex1, dat :: String
-ex1 = ""
+ex1 = unsafePerformIO (readFile "dayXXX-ex1.txt")
 dat = unsafePerformIO (readFile "dayXXX.txt")
 
 ans1 :: _
