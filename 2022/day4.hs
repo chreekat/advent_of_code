d93b605a274b9d467fab8a0506594e49988e99c8
Bryan Richter <b@chreekat.net>
2022-12-18 10:27:25 +0200

    2022.4.1

---
+++ 2022/day4.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+
+import Tropes
+import SimpleParse
+
+ex1, dat :: String
+ex1 = unsafePerformIO (readFile "day4-ex1.txt")
+dat = unsafePerformIO (readFile "day4.txt")
+
+
+pAssignment = do
+    a <- integer
+    _ <- p_char '-'
+    b <- integer
+    pure (a,b)
+
+pPair = do
+    f <- pAssignment
+    _ <- p_char ','
+    g <- pAssignment
+    pure (f,g)
+
+overlap (a, b) (m, n)
+    = a <= m && b >= n
+    || m <= a && n >= b
+
+     
+
+ans1 :: _
+ans1
+    = length
+    . filter (uncurry overlap)
+    . map (parse' pPair)
+    . lines
+
+ans2 :: _
+ans2 = undefined
+
+-- ghcid needs this?
+main = undefined
