f6f8b7e2e1d4ca8498ad7be840b63851311c08cb
Bryan Richter <b@chreekat.net>
2022-12-18 10:30:40 +0200

    2022.4.2

--- 2022/day4.hs
+++ 2022/day4.hs
@@ -26,21 +26,30 @@ pPair = do
     g <- pAssignment
     pure (f,g)
 
-overlap (a, b) (m, n)
+contains (a, b) (m, n)
     = a <= m && b >= n
     || m <= a && n >= b
 
-     
+
+overlap (a, b) (m, n)
+    = a <= m && b >= m
+    || a <= n && b >= n
+    || m <= a && n >= a
+    || m <= b && n >= b
 
 ans1 :: _
 ans1
     = length
-    . filter (uncurry overlap)
+    . filter (uncurry contains)
     . map (parse' pPair)
     . lines
 
 ans2 :: _
-ans2 = undefined
+ans2
+    = length
+    . filter (uncurry overlap)
+    . map (parse' pPair)
+    . lines
 
 -- ghcid needs this?
 main = undefined
