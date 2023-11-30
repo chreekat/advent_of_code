881219e7d997ad1dceff0618c3601510d1161aa8
Bryan Richter <b@chreekat.net>
2023-11-30 11:52:03 +0200

    Add hls

--- 2023/flake.nix
+++ 2023/flake.nix
@@ -1,8 +1,14 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   outputs = { self, nixpkgs }: 
-    let pkgs = import nixpkgs { system = "x86_64-linux"; }; in
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      shellPackages = p: [
+        p.advent-of-code-api
+        p.haskell-language-server
+      ];
+    in
     {
-      devShells.x86_64-linux.default = pkgs.haskellPackages.ghcWithPackages (p: [p.advent-of-code-api]);
+      devShells.x86_64-linux.default = pkgs.haskellPackages.ghcWithPackages shellPackages;
     };
 }
