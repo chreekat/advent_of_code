1f50827dd408249cec2aa66b8daa017f6a0f762a
Bryan Richter <b@chreekat.net>
2023-12-01 13:35:22 +0200

    Fix 2023.1

--- 2023/day1.hs
+++ 2023/day1.hs
@@ -22,35 +22,50 @@ dat = unsafePerformIO (readFile "day1.txt")
 
 ans1 :: _
 ans1 = sum
-    . map getCalibration
+    . map (getCalibration . (\s -> (s,s)))
     $ lines dat
 
-getCalibration :: String -> Int
-getCalibration = (\(First (Just f), Last (Just l)) -> read [f,l])
-    . foldl' bump (First Nothing, Last Nothing)
-  where
-    bump (f, l) x | x `elem` "123456789" = (f <> pure x, l <> pure x)
-                  | otherwise = (f, l)
+getCalibration :: (String, String) -> Int
+getCalibration (s, s') = read [getL s, getR s']
+
+getL = head . filter (`elem` "123456789")
+getR = last . filter (`elem` "123456789")
 
 ans2 :: _
 ans2 = sum
-    . map (pTraceShowId . getCalibration . pTraceShowId . simplify)
+    . map (getCalibration . (simplifyL &&& simplifyR))
     $ lines dat
 
-simplify :: String -> String
-simplify = go . pTraceShowId
+simplifyL :: String -> String
+simplifyL = go
   where
-    go ('o':'n':'e':xs)         = '1':simplify xs          
-    go ('t':'w':'o':xs)         = '2':simplify xs          
-    go ('t':'h':'r':'e':'e':xs) = '3':simplify xs          
-    go ('f':'o':'u':'r':xs)     = '4':simplify xs          
-    go ('f':'i':'v':'e':xs)     = '5':simplify xs          
-    go ('s':'i':'x':xs)         = '6':simplify xs          
-    go ('s':'e':'v':'e':'n':xs) = '7':simplify xs          
-    go ('e':'i':'g':'h':'t':xs) = '8':simplify xs          
-    go ('n':'i':'n':'e':xs)     = '9':simplify xs          
-    go (x:xs)                   = x  :simplify xs
-    go []                       = []                       
+    go ('o':'n':'e':xs)         = '1':simplifyL xs
+    go ('t':'w':'o':xs)         = '2':simplifyL xs
+    go ('t':'h':'r':'e':'e':xs) = '3':simplifyL xs
+    go ('f':'o':'u':'r':xs)     = '4':simplifyL xs
+    go ('f':'i':'v':'e':xs)     = '5':simplifyL xs
+    go ('s':'i':'x':xs)         = '6':simplifyL xs
+    go ('s':'e':'v':'e':'n':xs) = '7':simplifyL xs
+    go ('e':'i':'g':'h':'t':xs) = '8':simplifyL xs
+    go ('n':'i':'n':'e':xs)     = '9':simplifyL xs
+    go (x:xs)                   = x  :simplifyL xs
+    go []                       = []
+
+simplifyR :: String -> String
+simplifyR = reverse . go . reverse
+  where
+    go ('e':'n':'o':xs)         = '1':go xs
+    go ('o':'w':'t':xs)         = '2':go xs
+    go ('e':'e':'r':'h':'t':xs) = '3':go xs
+    go ('r':'u':'o':'f':xs)     = '4':go xs
+    go ('e':'v':'i':'f':xs)     = '5':go xs
+    go ('x':'i':'s':xs)         = '6':go xs
+    go ('n':'e':'v':'e':'s':xs) = '7':go xs
+    go ('t':'h':'g':'i':'e':xs) = '8':go xs
+    go ('e':'n':'i':'n':xs)     = '9':go xs
+    go (x:xs)                   = x  :go xs
+    go []                       = []
+
 
 -- ghcid needs this?
 main = print ans2
