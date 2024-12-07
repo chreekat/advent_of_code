e51f817f7e5b933dd53a77c809687cd361e79a92
Bryan Richter <b@chreekat.net>
2024-12-07 20:44:19 +0200

    Start things for new year

---
+++ 2024/template.hs
@@ -0,0 +1,32 @@
+{-# HLINT ignore "Move brackets to avoid $" #-}
+{-# HLINT ignore "Redundant map" #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+{-# HLINT ignore "Use infix" #-}
+{-# HLINT ignore "Use tuple-section" #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wno-deprecations #-}
+{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+import Tropes
+
+ex1, dat :: String
+{-# NOINLINE ex1 #-}
+ex1 = unsafePerformIO (readFile "XXX-ex1.txt")
+{-# NOINLINE dat #-}
+dat = unsafePerformIO (readFile "XXX.txt")
+
+ans1 :: _
+ans1 = ex1
+
+ans2 :: _
+ans2 = undefined
+
+main = print ans1
---
+++ template/Tropes.hs
@@ -0,0 +1,142 @@
+module Tropes (chunksOf, traceM, pTraceM, pTraceShowM, catMaybes, Map.keysSet, zipWithM, evalState, get, coerce, Max (..), isAlpha, isSymbol, isDigit, isUpper, Set, Sum (..), replicateM, replicateM_, execState, second, runState, (<=<), traverse_, gets, State (..), modify, mapMaybe, NE.NonEmpty (..), nub, toList, first, (Map.!), Map.mapWithKey, join, Comonad (..), Map.keys, fromJust, findIndices, findIndex, comparing, sortBy, (\\), minimumBy, genericLength, Map.fromListWith, Map.mapKeysWith, Map.Map, iterate', group, (***), (&&&), pTraceShowId, on, sort, fromMaybe, partition, intersect, isJust, isNothing, Last (..), pTraceShow, First (..), pPrint, All (..), intercalate, splitOn, traceShow, traceShowId, foldl', transpose, unsafePerformIO, elemIndex, module Tropes, Vec.ifoldl', Vec.slice) where
+
+import Control.Arrow
+import Control.Comonad
+import Control.Monad
+import Control.Monad.State
+import Control.Monad.Writer hiding (First, Last)
+import Data.Bool
+import Data.Char
+import Data.Coerce
+import Data.Foldable
+import Data.Function
+import Data.Ord
+import Data.List
+import qualified Data.List.NonEmpty as NE
+import Data.List.Split
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.Monoid (First (..), Last (..))
+import Data.Semigroup hiding (First, Last)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import System.IO.Unsafe
+import Text.Pretty.Simple
+import qualified Data.Vector as Vec
+import Data.Vector (Vector)
+
+import Debug.Pretty.Simple
+import Debug.Trace
+import Data.Bits
+
+-- Paterson always used irrefutable patterns for tuples. Why?
+_3to2 (a, b, c) = (a, b)
+_1of3 (a, _, _) = a
+_2of3 (_, b, _) = b
+_3of3 (_, _, c) = c
+
+-- @toBits id [True, True, False] = 6@
+toBits :: Bits bits => (a -> Bool) -> [a] -> bits
+toBits f = foldr (\(x, a) bits -> if f a then setBit bits x else bits) zeroBits . reverse . zip [0 ..] . reverse
+
+fromBits :: FiniteBits bits => (Bool -> a) -> Int -> bits -> [a]
+fromBits toA len b = pushBit (0, [])
+    where
+        pushBit (n, as) | n < len = pushBit (succ n, toA (testBit b n) : as)
+                        | otherwise = as
+
+hammingDistance :: Bits b => b -> b -> Int
+hammingDistance x = popCount . xor x
+
+assoc ((a, b), c) = (a, (b, c))
+
+avg :: Fractional a => [a] -> a
+avg xs = sum xs / genericLength xs
+
+mapAdjust :: Ord k => (a -> a) -> k -> Map k a -> Map k a
+mapAdjust = Map.adjust
+
+mapAlter :: Ord k => (Maybe a -> Maybe a) -> k -> Map.Map k a -> Map.Map k a
+mapAlter = Map.alter
+
+mapDelete :: Ord k => k -> Map k a -> Map k a
+mapDelete = Map.delete
+
+mapEmpty = Map.empty
+
+mapFilter = Map.filter
+
+mapFromList :: Ord k => [(k, a)] -> Map.Map k a
+mapFromList = Map.fromList
+
+mapInsert :: Ord k => k -> a -> Map k a -> Map k a
+mapInsert = Map.insert
+
+mapInsertMany :: Ord k => Map k a -> [(k, a)] -> Map k a
+mapInsertMany = foldr (uncurry Map.insert)
+
+mapLookup :: Ord k => k -> Map k a -> Maybe a
+mapLookup = Map.lookup
+
+mapSingleton = Map.singleton
+
+mapSize = Map.size
+
+mapToList = Map.toList
+
+mapUnionsWith :: (Foldable f, Ord k) => (a -> a -> a) -> f (Map.Map k a) -> Map.Map k a
+mapUnionsWith = Map.unionsWith
+
+mapUnionWith :: Ord k => (a -> a -> a) -> Map.Map k a -> Map.Map k a -> Map.Map k a
+mapUnionWith = Map.unionWith
+
+mapUnions :: Ord k => [Map.Map k a] -> Map.Map k a
+mapUnions = Map.unions
+
+median xs =
+    let n = length xs
+     in sort xs !! max 0 (n `div` 2)
+
+pars = splitOn "\n\n"
+prod2 (a, b) = product [a, b]
+
+range x y = if x > y then reverse [y .. x] else [x .. y]
+
+setEmpty = Set.empty
+
+setFromList :: Ord a => [a] -> Set a
+setFromList = Set.fromList
+
+setMap :: Ord k => (a -> k) -> Set a -> Set k
+setMap = Set.map
+
+setMember :: Ord k => k -> Set k -> Bool
+setMember = Set.member
+
+setPartition = Set.partition
+
+setSingleton = Set.singleton
+
+setSize = Set.size
+setUnion :: Ord k => Set k -> Set k -> Set k
+setUnion = Set.union
+
+sum2 (a, b) = sum [a, b]
+sum3 (a, b, c) = sum [a, b, c]
+
+unassoc (a, (b, c)) = ((a, b), c)
+
+unpars = intercalate "\n\n"
+
+mapKeysSet = Map.keysSet
+
+setDelete :: Ord a => a -> Set a -> Set a
+setDelete = Set.delete
+
+intersects :: (Foldable f, Eq a) => f [a] -> [a]
+intersects = foldr1 intersect
+
+vecUpdate = (Vec.//)
+veci = (Vec.!)
+vecFromList = Vec.fromList
