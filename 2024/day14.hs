62b2a4bfee5b30d2a88ce8dfed5afa9f28d85ae2
Bryan Richter <b@chreekat.net>
2024-12-19 15:32:14 +0200

    Giving up on 2024.14.2

--- 2024/day14.hs
+++ 2024/day14.hs
@@ -34,6 +34,8 @@ import Tropes qualified
 import TwoD qualified
 import Data.List.Split qualified as Split
 import SimpleParse qualified as Parse
+import Data.Hashable qualified as Hash
+import Debug.Trace qualified as Trace
 
 ex1, dat :: String
 {-# NOINLINE ex1 #-}
@@ -48,28 +50,68 @@ ans1 =
         Right robots = Parse.parse (Parse.many parse) mempty dat
         result = sort $ map (\(p,v) -> moveMany space p v count) robots
         quads = quadrants space result
-    in product $ map length quads
+        step m = map (\(p,v) -> moveMany space p v m)
+    in findLoop space (step start robots)
+    --in prettyState start space $ step start robots
+    --in product $ map length $ quadrants space (step 50 (step 25 (step 25 robots)))
+
+type State = [((Int, Int), (Int, Int))]
+
+-- fold over positions in a state on the x axis
+-- mirror (5,5) [((4,0),v)] = [((0,0),v)]
+-- mirror (5,5) [((3,1),v)] = [((1,1),v)]
+mirror :: (Int, Int) -> State -> State
+mirror (bx,_) = map g where
+    g ((x,y),v) = ((bx-x-1,y),v)
+
+start = 6243
+jump  = 1
+-- start = 16646
+-- jump  = 20806
+
+findLoop space = fix f Set.empty start where
+    step = map (\(p,v) -> moveMany space p v jump)
+    f nxt saved count state =
+        let [nw,ne,sw,se] = quadrants space state
+            h = Hash.hash state
+        --in if nw == mirror space ne && sw == mirror space se
+        --    then (count, state)
+        --    else if count > 20807 then (0, state) else nxt saved (succ count) (step state)
+        in Trace.trace (prettyState count space state) $
+            if Set.member h saved
+            then start + jump * Set.size saved
+            else
+                nxt (Set.insert h saved) ( count + jump) (step state)
+
+prettyState c (bx,by) robots = 
+    let ps = map fst robots
+        draw i
+            | i `elem` ps = '#'
+            | otherwise = '.'
+    in unlines (("HI! " <> show c) : [ [draw (x,y) | x <- [0..bx-1]] | y <- [0..by-1]])
 
 quadrants (bx,by) robots =
     let halfX = bx `div` 2
         halfY = by `div` 2
     in
-        [ [r | r@(x,y) <- robots, x < halfX, y < halfY]
-        , [r | r@(x,y) <- robots, x > halfX, y < halfY]
-        , [r | r@(x,y) <- robots, x < halfX, y > halfY]
-        , [r | r@(x,y) <- robots, x > halfX, y > halfY]]
+        [ [r | r@((x,y),_) <- robots, x < halfX, y < halfY]
+        , [r | r@((x,y),_) <- robots, x > halfX, y < halfY]
+        , [r | r@((x,y),_) <- robots, x < halfX, y > halfY]
+        , [r | r@((x,y),_) <- robots, x > halfX, y > halfY]]
 
 -- Move a robot within a space. They wrap, so use rem.
-move :: (Int, Int) -> (Int, Int) -> (Int, Int) -> (Int, Int)
-move (bx, by) (x, y) (dx, dy) = (x', y')
+move :: (Int, Int) -> (Int, Int) -> (Int, Int) -> _
+move (bx, by) (x, y) (dx, dy) = ((x',y'), (dx,dy))
   where
     adjustNeg b n = if n < 0 then b + n else n
     x' = adjustNeg bx $ (x + dx) `rem` bx
     y' = adjustNeg by $ (y + dy) `rem` by
 
 -- Move them multiple times.
-moveMany :: (Int, Int) -> (Int, Int) -> (Int, Int) -> Int -> (Int,Int)
-moveMany b p (dx,dy) c = move b p (dx*c, dy*c)
+moveMany :: (Int, Int) -> (Int, Int) -> (Int, Int) -> Int -> _
+moveMany b p v@(dx,dy) c = 
+    let (newP,_) = move b p (dx*c, dy*c)
+    in (newP,v)
 
 parse = do
     _ <- Parse.string "p="
